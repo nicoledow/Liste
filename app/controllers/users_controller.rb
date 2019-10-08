c67c5ce7e14a298aa8dc4b1f7a58687d84b6cfe3
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 12:52:38 -0400

    set up in-form validations for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    #binding.pry
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Thank you! You have successfully signed up!"
       redirect_to lists_path
+    else 
+      render :new
     end
   end
 
@@ -17,6 +19,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :manager)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, presence: true
-    validates :email, presence: true
+    validates_presence_of :email
     validates :email, uniqueness: { message: "Email already in use."}
-    validates :password, length: { minimum: 5, message: "Password must be at least five characters." }
+    validates :password, length: { minimum: 5, message: "must be at least five characters." }
 
     has_many :assignments
     has_many :tasks, through: :assignments
