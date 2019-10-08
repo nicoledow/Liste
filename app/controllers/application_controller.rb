d94317bcd6c6e6fbc525e1192fca438ed064c84e
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 14:53:01 -0400

    require login before most actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
+    before_action :require_login
+    
     def home
+    end
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def require_login
+      if !session[:user_id]
+        flash[:login_required] = "Log in required."
+        redirect_to root
+      end
     end
 end
