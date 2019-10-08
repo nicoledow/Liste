e1bfa6ba675ad38e433a002f4185f21a9b001262
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 13:58:39 -0400

    current user and require login methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def require_login
+      if !session[:user_id]
+        flash[:login_required] = "Log in required."
+        redirect_to root
+      end
+    end
 end
