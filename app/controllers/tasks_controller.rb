192654ac47f4c7bafc366715261c83166ca3ef56
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 18:31:59 -0400

    changed title based on list_task show or task index route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,14 @@ class TasksController < ApplicationController
   def index
     if params[:list_id]
       @tasks = List.find(params[:list_id]).tasks
+      @list = List.find(params[:list_id])
     else
       @tasks = Task.all
     end
+  end
+
+  def create
+    raise params.inspect
   end
 
 
