00bf0675ee4467434edc77135247ac2138e579db
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 13:31:46 -0400

    configured route for existing user to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,17 @@
 class SessionsController < ApplicationController
-  def new
-  end
+    def new
+    end
 
-  def create
-    raise params.inspect
-  end
+    def create
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to lists_path
+      else
+        flash[:failure] = "Email or password was incorrect. Please try again."
+        render :new
+      end
+    end
 
-end
+
+  end
