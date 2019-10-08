7e87ac938437a1b494a759a3736fcf574b90bbe5
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 13:00:33 -0400

    configuring route for existing user to log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'application#home'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   
   resources :users, only: [:new, :create]
 
