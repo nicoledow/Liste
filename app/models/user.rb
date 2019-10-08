69b107312c044e2c83f47ba407758a74be08e54a
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 10:20:42 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 5 }
+
     has_many :assignments
     has_many :tasks, through: :assignments
+
+    def manager?
+        self.manager == true
+    end
 end
