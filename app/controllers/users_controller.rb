f5f77468dc254cec3e932e39cded62cf63efcba5
Nicole Dow <nikki.dow71@gmail.com>
2019-10-08 10:56:19 -0400

    working on logic for creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    raise params.inspect
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Thank you! You have successfully signed up!"
+      redirect_to lists_path
+    end
+  end
 
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
