2ab17146ec9ce44ac92964fc6800c929adbfb8b6
Nicole Dow <nikki.dow71@gmail.com>
2019-10-09 11:12:26 -0400

    fixed infinite loop on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :require_login
+    skip_before_action :require_login, only: [:home]
     helper_method :current_user
     helper_method :logged_in?
     helper_method :require_login
@@ -19,8 +20,8 @@ class ApplicationController < ActionController::Base
 
     def require_login
       if !session[:user_id]
-        flash[:login_required] = "Log in required."
-        redirect_to root
+        flash[:login_required] = "Log in required." 
+        redirect_to root_path
       end
     end
 end
