a9ce9acbbd7932fb5382a7cd31d597751b147c02
Nicole Dow <nikki.dow71@gmail.com>
2019-10-09 12:18:06 -0400

    added 'assigned?' instance method

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,9 @@ class Task < ApplicationRecord
     has_many :assignments
     belongs_to :list
     has_many :notes
+
+
+    def assigned?
+      Assignment.find_by(task_id: self.id) ? true : false
+    end
 end
