4e8ba6f08324835634e2406a772480e092e658c9
Nicole Dow <nikki.dow71@gmail.com>
2020-02-11 19:08:41 -0700

    add test for user email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,12 @@ RSpec.describe User, type: :model do
         expect(subject).to_not be_valid
     end
 
+    it 'is invalid when the email is already in use' do
+        User.create(name: "Nikki", email: "nikki@test.com", password: "donuts")
+        user2 = User.new(name: "Nikki2", email: "nikki@test.com", password: "donuts")
+        expect(user2).to_not be_valid
+    end
+
     it 'should have a password' do
         subject.password = nil
         expect(subject).to_not be_valid
